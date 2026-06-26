0b061fbfc7c99ce7658495395423f7308874e63c
DavidSun-11 <1712018019@qq.com>
2026-06-26 17:29:01 +0800

    Fix Live2D admin entry placement

--- assets/live2d-interactions.js
+++ assets/live2d-interactions.js
@@ -6,7 +6,7 @@
 
     window.__JUNXUE_LIVE2D_INTERACTIONS_INSTALLED__ = true;
 
-    const LAZY_SCRIPT_SRC = "assets/live2d-interactions-lazy.js?v=20260626-boss-profile-script-fix1";
+    const LAZY_SCRIPT_SRC = "assets/live2d-interactions-lazy.js?v=20260626-live2d-admin-main-entry1";
     if (typeof window.enableGanyuMemory !== "boolean") {
         window.enableGanyuMemory = true;
     }

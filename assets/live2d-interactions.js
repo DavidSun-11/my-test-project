6202697953a86d07db20c7b1cf7070b098e5d28b
DavidSun-11 <1712018019@qq.com>
2026-06-26 11:06:11 +0800

    Refine Live2D menu ripple spacing

--- assets/live2d-interactions.js
+++ assets/live2d-interactions.js
@@ -6,7 +6,7 @@
 
     window.__JUNXUE_LIVE2D_INTERACTIONS_INSTALLED__ = true;
 
-    const LAZY_SCRIPT_SRC = "assets/live2d-interactions-lazy.js?v=20260624-live2d-menu-beautiful-replica5";
+    const LAZY_SCRIPT_SRC = "assets/live2d-interactions-lazy.js?v=20260624-live2d-menu-beautiful-replica6";
     if (typeof window.enableGanyuMemory !== "boolean") {
         window.enableGanyuMemory = true;
     }
